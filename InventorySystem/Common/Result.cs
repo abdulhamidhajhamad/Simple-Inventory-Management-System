4084c0e06b49b251a623a21326c816cda383f02d
abdulhamidhajhamad <abdulhamidhajhamad@gmail.com>
2026-06-10 14:51:07 -0700

    update result to accept generic

--- InventorySystem/Common/Result.cs
+++ InventorySystem/Common/Result.cs
@@ -3,6 +3,7 @@ namespace InventorySystem.Common;
 public class Result
 {
     public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
     public string ErrorMessage { get; }
 
     protected Result(bool isSuccess, string errorMessage) => 
@@ -10,4 +11,18 @@ public class Result
 
     public static Result Success() => new(true, string.Empty);
     public static Result Failure(string message) => new(false, message);
+}
+
+public class Result<T> : Result
+{
+    public T? Value { get; }
+
+    private Result(bool isSuccess, T? value, string errorMessage) 
+        : base(isSuccess, errorMessage)
+    {
+        Value = value;
+    }
+
+    public static Result<T> Success(T value) => new(true, value, string.Empty);
+    public static new Result<T> Failure(string message) => new(false, default, message);
 }
