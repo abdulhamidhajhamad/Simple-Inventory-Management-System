6139b3d26c8a80a7758628436ebafad88235bdec
abdulhamidhajhamad <abdulhamidhajhamad@gmail.com>
2026-06-10 16:04:06 -0700

    writing program to test adding feature

--- InventorySystem/Program.cs
+++ InventorySystem/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using InventorySystem.Repositories;
+using InventorySystem.Services;
+using InventorySystem.UI;
+
+namespace InventorySystem;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Advanced Inventory Management System - Phase 1";
+
+        IProductRepository repository = new InMemoryProductRepository();
+
+        IInventoryService inventoryService = new InventoryService(repository);
+
+        ConsoleUI ui = new ConsoleUI(inventoryService);
+
+        ui.Run();
+    }
+}
